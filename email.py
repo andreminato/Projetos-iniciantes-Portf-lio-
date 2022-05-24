3c5cefa59975ec3dc899db5713eb23e83de9dfbd
andreminato <105604050+andreminato@users.noreply.github.com>
2022-05-24 12:13:12 -0300

    Update email.py

--- email.py
+++ email.py
@@ -14,8 +14,8 @@ info = pyautogui
 email.To = 'aminato40@outlook.com; jose@bol.com'
 email.Subject = 'Email automático do André'
 email.HTMLBody = f'<p> O texto aqui pode ser formatato conforme a necessidade </p>'
-'<p> As variáveris {v1, v2} podem ser informações extraídas de algum BD ou arquivo. <p>'
-'<p> Também tem a possibilidade de automatizar o processo de downloado dos dados com a extensão {pyautogui} </p>'
+f'<p> As variáveris {v1, v2} podem ser informações extraídas de algum BD ou arquivo. <p>'
+f'<p> Também tem a possibilidade de automatizar o processo de downloado dos dados com a extensão {info} </p>'
 
 email.Send()
 print('Email enviado')
